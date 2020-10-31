61b7e38827a42cfad0e0f5b1fa36696a57676126
vladdamian <vlad.damian@vspartners.us>
2020-10-31 21:29:16 +0200

    another commit

--- functions/index.js
+++ functions/index.js
@@ -5,5 +5,5 @@ const functions = require('firebase-functions');
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase - cratita rosie");
+  response.send("Hello from Firebase - something else§");
 });
